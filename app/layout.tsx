0d2a26e8bc1e30f040ed65356e5b89da8c67280e
jrjlawas <jayromellawas30@gmail.com>
2025-11-25 11:47:56 +0800

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import "@/public/styles/style.scss";
+import InitAnimations from "@/components/layout/InitAnimations";
+
+export const metadata: Metadata = {
+  title: "Gratech | Software Development & IT Solutions React NextJs Template",
+  description: "Software Development & IT Solutions React NextJs Template",
+  keywords: [
+    "Software",
+    "IT",
+    "Agency",
+    "Javascript",
+    "Typescript",
+    "React",
+    "nextjs",
+  ],
+  authors: [
+    {
+      name: "Gramentheme",
+      url: "https://themeforest.net/user/gramentheme",
+    },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <InitAnimations />
+      </body>
+    </html>
+  );
+}
